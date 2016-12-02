b435b5601a753f92afbc42541ddfdf22103324e7
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-12-02 01:44:00 -0500

    convertlist

--- x_code/convert_list.py
+++ x_code/convert_list.py
@@ -48,7 +48,7 @@ with open(fileout, 'w') as f:
 
 		dis = dis * dis
 		dis = np.sum(dis, 1)
-		cid = np.argmax(dis) + 1
+		cid = np.argmin(dis) + 1
 
 		f.write('{0} {1} {2}\n'.format(filename1, filename2, cid) )
 
