66dffbda16f9ca95f6bfe05274a03aa5cbe4b3c8
Xiaolong Wang <xiaolonw@cs.cmu.edu>
2016-12-05 14:46:13 -0500

    test

--- action_cls/test_vis_error.lua
+++ action_cls/test_vis_error.lua
@@ -65,7 +65,7 @@ torch.setdefaulttensortype('torch.CudaTensor')
 
 
 
-model_G = torch.load('/scratch/xiaolonw/models_ucf/train_hmdb_cls_frame1/14_net_D.t7')
+model_G = torch.load('/scratch/xiaolonw/models_ucf/train_hmdb_cls_frame1/32_net_D.t7')
 model_G:evaluate()
 
 
